0675fbe71f6637358f2e5549b6bac346cf918a44
Jonathan <new.jonathan01@gmail.com>
2020-07-17 01:44:18 -0300

    Nano

---
+++ Nano/src/test/java/com/nanoblog/Nano/NanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanoblog.Nano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
